e302abba0a8c16465569143005406c6da6d911da
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-21 10:54:41 -0400

    index auto made default choice const and i still have the error but i dont know what it is

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 const sql = require("./db/connect");
 const helper = require("./lib/helper")
 
@@ -8,14 +9,7 @@ const business = await inquirer.prompt([
         type:'input',
         name: 'name',
         message: 'what is the name of your business?',
-        validate: (name) =>{
-            if (name) {
-              return true;
-            } else {
-              console.log(" Please Enter a business Name")
-              return false;
-            }
-    },
+
 },
 ]);
 await sql.addBusiness(business);
@@ -28,7 +22,24 @@ const newEployee = async () => {
     const employee = await inquirer.prompt([
         {
             type: 'input',
-
+            name: 'first_name',
+            message: 'what is the Employees first name?'
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'what is the employees role',
+            Choices: roleAdd,
+            loop: false,
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'who is the employees manager?',
+            Choices: roleAdd,
+            loop: false,
         }
-    ])
+    ]);
+    await sql.addEmployee(employee);
+    chooseRequest();
 }
