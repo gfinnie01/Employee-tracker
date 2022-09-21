7daf4523f708274467cb0da84d451279cf097f72
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-20 22:07:45 -0400

    think i got a good outline for a querry

---
+++ db/querry.sql
@@ -0,0 +1,34 @@
+const db = require('./connection');
+
+class GNTQuery {
+constroctor(db){
+    this.db =db;
+}
+addDept(data) {
+    const values = [data.name];
+    return this.db
+    .promise()
+    .query(
+        INSERT INTO business (business_name) VALUES(?),
+        values
+    );
+}
+addRole(data) {
+    consr values = [data.title, data.salary, data.business_id]
+    return this.db
+    .promise()
+    .query(
+        INSERT INTO role (title, salary, business_id) VALUES(?,?,?),
+        values
+    );
+}
+addEmployee(data) {
+    const values = [data.employee_first_name, data.employee_last_name, data.role_id, data.manager_id];
+    return this.db
+    .promise()
+    .query(
+        INSERT INTO Employee (employee_first_name, employee_last_name, role_id, manager_id)
+        VALUES(?,?,?,?)
+    )
+}
+}
